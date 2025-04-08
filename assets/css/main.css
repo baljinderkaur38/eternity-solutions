f9e44fc0625c94adbab61c5bb9907eb6c89f6b9f
baljinderkaur38 <51135094+baljinderkaur38@users.noreply.github.com>
2025-04-08 18:19:33 +0530

    basic changes

--- assets/css/main.css
+++ assets/css/main.css
@@ -1,6 +1,6 @@
 /**
-* Template Name: Arsha
-* Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+* Template Name: Eternity Solutions
+* Template URL: https://bootstrapmade.com/Eternity Solutions-free-bootstrap-html-template-corporate/
 * Updated: Feb 22 2025 with Bootstrap v5.3.3
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
