708fbdd0ad8a86449e2fe9d5d3d27693b6d4b2db
baljinderkaur38 <51135094+baljinderkaur38@users.noreply.github.com>
2025-05-07 15:41:01 +0530

    contact

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  echo "die";die;
+
   $receiving_email_address = 'baljinderkau1374@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
